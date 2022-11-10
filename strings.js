8dec91fc3f0768cb3416a3e50425272d8a5a3366
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-10 08:41:58 -0300

    atualização de conhecimentos, usando strings

--- strings.js
+++ strings.js
@@ -6,3 +6,8 @@ console.log(escola.charCodeAt(3)) // exibe o código unicode do caracter
 
 console.log(escola.substring(1)) // exibe a partir do caracter selecionado
 console.log(escola.substring(1,4)) // exibe a partir do caracter 1 até o 3
+
+console.log('Escola '.concat(escola).concat("!"))  // função para concatenar
+console.log(escola.replace('k', 'K')) // substitui o k minúsculo por um maiúsculo
+
+console.log('Ana, Maria, João'.split(',')) // Transforma a string em Array a partir da vírgula
