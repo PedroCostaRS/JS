7ece940337b91d9ffa88b4d2a60785317dbcec7f
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-10 08:52:00 -0300

    atualização de conhecimentos, usando template string

---
+++ templateString.js
@@ -0,0 +1,22 @@
+const nome = 'Rebecca'
+
+const concatena = "Olá " + nome + "!" // forma antiga para se concatenar string
+
+const template = `
+    Olá
+    ${nome}!`
+
+    /*
+    * forma onde tudo que está entre crase é considerado na string
+    * implementado a partir do ecmascript 'js' 2015
+    * 
+    *  o símbolo do dólar e chaves interpola/interpreta strings
+    *  tudo dentro dessa superfunção é interpretado, seja soma, string ou array
+    *  chamada de método, funções ou qualquer que seja a expressão
+    */
+
+console.log(concatena, template)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei.. ${up("cuidado")}!`) // chama a função up que converte texto para maiúsculo
+ 
