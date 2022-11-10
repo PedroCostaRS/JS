e736d3dcb255f7267f72a13a725c62820fd6fad6
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-09 23:38:49 -0300

    atualização de conhecimentos, usando strings

---
+++ strings.js
@@ -0,0 +1,8 @@
+const escola = "Sockel"
+
+console.log(escola.charAt(4)) // como inicia no 0 retorna o quinto elemento 
+console.log(escola.charAt(6)) // se não encontra retorna valor vazio
+console.log(escola.charCodeAt(3)) // exibe o código unicode do caracter
+
+console.log(escola.substring(1)) // exibe a partir do caracter selecionado
+console.log(escola.substring(1,4)) // exibe a partir do caracter 1 até o 3
