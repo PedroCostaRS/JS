b78f9ce69b689368f57b55edfead884c571b6306
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-09 23:26:24 -0300

    atualização de conhecimentos

---
+++ atualizacoesConhecimentos.js
@@ -0,0 +1,7 @@
+console.log(7/0)  // retorna valor infinito
+console.log("10"/2)  // verifica se a string é number e efetua a divisão
+console.log("Teste"*2) // verifica se a string é number e retorna NaN
+console.log(0.1 + 0.7) // soma retornando valor quebrado padrão IEEE otimiza, mas gera imprecisão
+//console.log(10.toString()) // retorna erro de conversão por ser literal
+console.log((10).toString) // retorna função de conversão para string
+console.log((10.345).toFixed(2)) // retorna apenas duas casas decimais
