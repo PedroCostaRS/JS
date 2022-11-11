aaf05ad24bce2f2365ce1028cab5c27a1a3decb7
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-11 20:13:56 -0300

    Relembrando JS

---
+++ usandoVarEmLoop2.js
@@ -0,0 +1,14 @@
+const funcs = []
+
+for (var index = 0; index < 10; index++) {
+    funcs.push(function () {
+        console.log(index)
+    })
+}
+
+funcs[2]()
+funcs[8]()
+
+// usando var ele não acessa a função em cada iteração
+// só respeita a ultima iteração
+// Já com LET ele respeita todas as iterações
