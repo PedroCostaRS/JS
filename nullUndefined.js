b81886b2e92b4052971fa64a5f8de1b6004ee2cc
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-10 22:09:39 -0300

    Relembrando JS

---
+++ nullUndefined.js
@@ -0,0 +1,11 @@
+let valor // não foi definida 
+console.log(valor) // retorna Undefined (indefinida)
+
+// console.log(valor1) // is not defined (não foi definida/declarada)
+valor = null // inicializou com vazio valor null(nulo)
+console.log(valor)
+
+/*
+Programador usa nulo (null)
+Aplicação usa o Undefined (indefinido)
+*/
