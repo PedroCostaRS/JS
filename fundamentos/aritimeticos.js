7e3fb70d1b1dbc85f4195fb74a18302f5bd917c1
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-13 14:51:09 -0300

    Relembrando JS

---
+++ fundamentos/aritimeticos.js
@@ -0,0 +1,12 @@
+const [a, b, c, d] = [1, 2, 3, 4]
+const soma = a + b + c + d
+const subtracao = d - c - b - a
+const mult = a * b
+const divis = d / b
+const modulo = d % a
+
+console.log(soma)
+console.log(subtracao)
+console.log(mult)
+console.log(divis)
+console.log(modulo)
