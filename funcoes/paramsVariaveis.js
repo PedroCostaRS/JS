54e204163e82165c6ea13787f542ffec82204397
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-18 23:02:57 -0300

    Relembrando JS

---
+++ funcoes/paramsVariaveis.js
@@ -0,0 +1,15 @@
+// toda função tem o array arguments
+function soma() {
+    let soma = 0
+    for (i in arguments) {
+        soma += arguments[i]
+    }
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(6))
+console.log(soma(5))
+console.log(soma(1, 3, 5))
+console.log(soma(1, 3, 5, ' teste'))
