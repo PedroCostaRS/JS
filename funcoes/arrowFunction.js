bffffa86d58b22fdef9ecaaefc012564d1cab8fe
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-23 19:50:14 -0300

    Relembrando JS

--- funcoes/arrowFunction.js
+++ funcoes/arrowFunction.js
@@ -2,3 +2,16 @@ let dobro = function (a) {
     return 2 * a
 }
 console.log(dobro(10))
+
+dobro = (a) => { return 2 * a } // mesma função baseada em arrow function
+
+dobro = a => 2 * a // mesma função simplificada em arrow function
+
+
+// mais um exemplo de função
+let ola = function () {
+    return 'Olá'
+}
+
+ola = () => 'Olá' // mesma função simplificada em arrow function
+
