b57b26a99957c0d2ba1cae266f6cd4d572d87dfc
PedroCostaRS <tecpedrocosta@gmail.com>
2022-11-28 21:26:20 -0300

    Relembrando JS

--- funcoes/tiposDeclaracao.js
+++ funcoes/tiposDeclaracao.js
@@ -3,14 +3,14 @@
 // function declaration padrão
 function soma(a, b) {
     return a + b
-}
+}// usada em qualquer ponto do software, mesmo antes da sua declaração
 
 // function expression - retorna os valores para uma const ou let
 const sub = function (a, b) {
     return a - b
-}
+}// Só pode ser usada após a declaração
 
 // named function expression
 const mult = function mult(a, b) {
     return a * b
-}
+} // Só pode ser usada após a declaração
